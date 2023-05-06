b850515bda10d3a9137d122d1058ced3b5e44bff
Robert Vari <mail.robertvari@gmail.com>
2023-05-06 16:44:42 +0200

    update

--- 07_list_methods.py
+++ 07_list_methods.py
@@ -5,4 +5,18 @@ my_driends.append("Csaba")
 my_driends.append("Kriszta")
 my_driends.append("Tamás")
 
-print(my_driends[0])
+my_driends.insert(0, "Csilla")
+
+fruits = ["apple", "lemon", "banana"]
+my_driends.extend(fruits)
+
+# remove items from list
+print(my_driends)
+my_driends.remove('Tamás')
+
+del my_driends[0]
+print(my_driends)
+
+# update item
+my_driends[2] = "Béla"
+print(my_driends)
