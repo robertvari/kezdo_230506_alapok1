1a8df3c46351853cd41fc4e79b3c16a61eebb259
Robert Vari <mail.robertvari@gmail.com>
2023-05-06 15:38:40 +0200

    update

---
+++ 05_string_format.py
@@ -0,0 +1,7 @@
+name, address, email, age = "Kiss Csaba", "Budapest", "csaba@gmail.com", 32
+
+name = "Nagy Géza".upper()
+address = "Eger"
+
+# f = formated string
+print(f"Your name is {name}. \nYou live in {address}. \nYour email is {email}. \nYou are {age} old.")
