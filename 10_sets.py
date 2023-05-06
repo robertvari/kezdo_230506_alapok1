67af13089e4322ca230f5b1b55342431a8949e3d
Robert Vari <mail.robertvari@gmail.com>
2023-05-06 17:21:39 +0200

    update

---
+++ 10_sets.py
@@ -0,0 +1,29 @@
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+# union
+# print(a | b)
+
+# difference
+# print(a - b)
+# print(b - a)
+
+# intersection
+print(a & b)
+
+# symmetrical difference
+print(a ^ b)
+
+# add item
+a.add(10)
+print(a)
+
+a.update([11, 12, 13])
+print(a)
+
+# remove item
+a.remove(1)
+print(a)
+
+n = {1, 3, 4, 2}
+print(n)
