212761da006fe0acccdfdae58f798f6973b109a0
Robert Vari <mail.robertvari@gmail.com>
2023-05-06 15:04:12 +0200

    update

---
+++ 02_string_methods.py
@@ -0,0 +1,7 @@
+# print("Hello World".upper())
+# print("Hello World".lower())
+# print("hello world".title())
+# print("hello world".replace("world", "Csaba"))
+
+# command chaining
+print("hello world".replace("world", "Csaba").replace("hello", "Szia"))
