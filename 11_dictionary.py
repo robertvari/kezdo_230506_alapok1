d7ee9906f935b718e78a4e2e49b71bf57886fb1e
Robert Vari <mail.robertvari@gmail.com>
2023-05-06 17:43:31 +0200

    update

---
+++ 11_dictionary.py
@@ -0,0 +1,14 @@
+phonebook = {
+    "0620 1234 567": {"name": "Csaba", "address":"Budapest", "email": "csaba@gmail.com"},
+    "0620 2345 678": {"name": "Kriszta", "address":"Pécs", "email": "kriszta@gmail.com"},
+    "0620 3456 789": {"name": "Tamás", "address":"Miskolc", "email": "tamas@gmail.com"}
+}
+
+license_plates = {
+    "ABC-123": "BMW",
+    "DGR-456": "Peugeot",
+    "HOT-432": "Citroen"
+}
+
+print(phonebook["0620 1234 567"]["name"])
+# print(license_plates["HOT-432"])
