204775f2cc4d85b688ff4f95e0fa6109bea9a05f
Robert Vari <mail.robertvari@gmail.com>
2023-05-07 14:21:40 +0200

    update

---
+++ 13_conditions.py
@@ -0,0 +1,25 @@
+number = 2
+
+#     False
+if number == 10:
+    print("The number is == 10")
+
+#     True
+if number != 10:
+    print("The number is != 10")
+
+#     True
+if number < 10:
+    print("The number is < 10")
+
+#     False
+if number > 10:
+    print("The number is > 10")
+
+#     True
+if number <= 10:
+    print("The number is <= 10")
+
+#     False
+if number >= 10:
+    print("The number is >= 10")
