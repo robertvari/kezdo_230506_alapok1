fc0558303da8bb097549551e7ae5a13ed63d9971
Robert Vari <mail.robertvari@gmail.com>
2023-05-07 15:13:06 +0200

    update

---
+++ 20_for_loop.py
@@ -0,0 +1,12 @@
+my_friends = ["Csaba", "Kriszta", "Tamás", "Gergő", "Csilla", "Zoli"]
+fruits = ("Banana", "Apple", "Lemon")
+my_name = "Robert Vari"
+
+# for name in my_friends:
+#     print(f"Hello {name}")
+
+for froit in fruits:
+    print(f"My favorite is {froit}")
+
+# for character in my_name:
+#     print(character.upper())
